b738657d857d7ff8a33c7dabeed9245f3dff15cb
Yash Garg <yash1.garg@one97.delhi.net>
2025-04-16 14:34:44 +0530

    contact us completed

--- app/dashboard/questions/page.jsx
+++ app/dashboard/questions/page.jsx
@@ -1,18 +1,17 @@
 export default function QuestionsPage() {
     return (
-      <div className="p-4 space-y-4">
-        <h1 className="text-xl font-bold">Questions Page</h1>
-        <p>All your mock interview questions will show here.</p>
-  
-        <a
-          href="https://mail.google.com/mail/?view=cm&fs=1&to=yashgarg1701@gmail.com&su=Query%20about%20Mock%20Interview&body=Hi%20Yash,"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
-        >
-          Ask a Query
-        </a>
-      </div>
+        <div className="p-4 space-y-4">
+            <h1 className="text-xl font-bold">Questions Page</h1>
+            <p>All your mock interview questions will show here.</p>
+
+            <a
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=yashgarg1701@gmail.com&su=Query%20about%20Mock%20Interview&body=Hi%20Yash,"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
+            >z
+                Ask a Query
+            </a>
+        </div>
     );
-  }
-  
+}
